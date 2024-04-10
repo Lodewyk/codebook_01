065f6265879cfda0c26df09d1bee3d0a62bbc76e
Lodewyk Duminy <lodewyk.duminy@gmail.com>
2024-04-10 13:01:05 +1200

    Added hooks and some testing code

--- src/index.tsx
+++ src/index.tsx
@@ -2,15 +2,20 @@ import 'bulmaswatch/superhero/bulmaswatch.min.css';
 import ReactDOM from 'react-dom/client';
 // import CodeCell from './components/code-cell';
 import TextEditor from './components/text-editor';
+import { Provider } from 'react-redux';
+import { store } from './state';
+import CellList from './components/cell-list';
 
 const el = document.getElementById('root');
 const root = ReactDOM.createRoot(el!);
 
 const App = () => {
     return (
-        <div>
-            <TextEditor />
-        </div>
+        <Provider store={store}>
+            <div>
+                <CellList />
+            </div>
+        </Provider>
     );
 }
 
