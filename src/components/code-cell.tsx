ae2d12af2210688f0483a50d017f0c118069d515
Lodewyk Duminy <lodewyk.duminy@gmail.com>
2024-04-15 12:18:51 +1200

    Moved code concatenation and bundling to an external hook just to clean up the code a little

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -6,6 +6,7 @@ import { Cell } from '../state';
 import { useActions } from '../hooks/use-actions';
 import { useTypedSelector } from '../hooks/use-typed-selector';
 import './code-cell.css';
+import { useCumulativeCode } from '../hooks/use-cumulative-code';
 
 interface CodeCellProps {
     cell: Cell
@@ -14,67 +15,7 @@ interface CodeCellProps {
 const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
     const { updateCell, createBundle } = useActions();
     const bundle = useTypedSelector((state) => state.bundles[cell.id]);
-    /**
-     * This section joins together all the code that we have in this cell and the cells above
-     * so that we can bundle it all together, allowing users to access code written in previous
-     * cells.
-     */
-    const cumulativeCode = useTypedSelector((state) => {
-        const { data, order } = state.cells;
-        const orderedCells = order.map(id => data[id]);
-
-        /**
-         * This will be used to render the user code to the preview window
-         * 
-         * using 'var' for the declaration of show to allow us to declare it 
-         * multiple times
-         */
-        const showFunc = `
-            import _React from 'react';
-            import _ReactDOM from 'react-dom';
-            var show = (value) => {
-                const root = document.querySelector('#root');
-                if (typeof value === 'object') {
-                    if (value.$$typeof && value.props) {
-                        // render JSX component
-                        _ReactDOM.render(value, root);
-                    } else {
-                        // render JS object
-                        root.innerHTML = JSON.stringify(value);
-                    }
-                } else {
-                    // render JS
-                    root.innerHTML = value;
-                }
-            }
-        `;
-        const showFuncNoOperation = `var show = () => {}`;
-
-        let code = [];
-        for (let c of orderedCells) {
-            if (c.type === 'code') {
-                /**
-                 * This if statement checks whether or not the id of the cell is the current id.
-                 * if the cell we're cumulating is the current cell, we want to add the working 
-                 * `show` function, otherwise we add the non-operational function. 
-                 * This prevents code from previous cells where the show function is being called
-                 * from being rendered in the preview window of a later cell, which is confusing.
-                 */
-                if (c.id === cell.id) {
-                    code.push(showFunc);
-                } else {
-                    code.push(showFuncNoOperation);
-                }
-                code.push(c.content);
-            }
-
-            // only cumulate up until the current cell
-            if (c.id === cell.id) {
-                break;
-            }
-        }
-        return code;
-    });
+    const cumulativeCode = useCumulativeCode(cell.id);
 
     // this will ensure we only bundle if the user stops adding input for 1 second or longer
     useEffect(() => {
@@ -83,19 +24,19 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
          * the page loads
          */
         if (!bundle) {
-            createBundle(cell.id, cumulativeCode.join('\n'));
+            createBundle(cell.id, cumulativeCode);
             return;
         }
 
         const timer = setTimeout(async () => {
-            createBundle(cell.id, cumulativeCode.join('\n'));
+            createBundle(cell.id, cumulativeCode);
         }, 1000); // update timer here if you want to bundle faster, this increases CPU usage, be aware
 
         return () => {
             clearTimeout(timer);
         }
         // ** IMPORTANT ** do not add 'bundle' in here or it causes an infinite loop of bundling
-    }, [createBundle, cell.id, cumulativeCode.join('\n')]); // only runs when input changes
+    }, [createBundle, cell.id, cumulativeCode]); // only runs when input changes
 
     return (
         <Resizable direction="vertical">
